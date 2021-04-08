31dc05a6247c035c4e84e4486c2490f5a032433e
lyh7712 <mmj2rv9@kakao.com>
2021-04-08 20:17:13 +0900

    Initial commit

---
+++ src/main/java/lyh7712/orderservice/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package lyh7712.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lyh7712/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lyh7712.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
