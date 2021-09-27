cee061f67070817a1a9ced3c53507a62f0dca836
lyh7712 <mmj2rv9@kakao.com>
2021-09-27 16:26:25 +0900

    알고리즘

---
+++ src/main/java/lyh7712/Test.java
@@ -0,0 +1,32 @@
+package lyh7712;
+
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Test test = new Test();
+
+        String[] tests = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion = {"leo", "kiki"};
+
+        test.solution(tests, completion);
+
+    }
+    public String solution(String[] participant, String[] completion) {
+        int i;
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
+        }
+        return participant[i];
+    }
+
+}
+
