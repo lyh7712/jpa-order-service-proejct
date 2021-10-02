1088a08e15e3d8a53bc0f6f4810561e671e364da
lyh7712 <mmj2rv9@kakao.com>
2021-10-02 20:37:22 +0900

    알고리즘

--- src/main/java/lyh7712/Test.java
+++ src/main/java/lyh7712/Test.java
@@ -6,20 +6,26 @@ public class Test {
 
         Test test = new Test();
 
-        test.solution(8888);
+        int[][] arr1 = new int[][]{{1, 2}, {2, 3}};
+        int[][] arr2 = new int[][]{{3, 4}, {5, 6}};
 
+        System.out.println(arr1.length);
+        System.out.println(arr2.length);
+        System.out.println(arr1[0].length);
 
+        test.solution(arr1, arr2);
     }
-    public int solution(int n) {
-        int sum = 0;
 
-        while (n != 0) {
-            sum += n % 10;
-            n = n / 10;
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+
+            }
         }
-
-        System.out.println(sum);
-        return sum;
+        return answer;
     }
 
 }
