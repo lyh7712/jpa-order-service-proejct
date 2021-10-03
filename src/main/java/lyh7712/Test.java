8938e2900a47c792fb29920699d9b615a449b409
lyh7712 <mmj2rv9@kakao.com>
2021-10-03 15:11:03 +0900

    알고리즘

--- src/main/java/lyh7712/Test.java
+++ src/main/java/lyh7712/Test.java
@@ -1,30 +1,50 @@
 package lyh7712;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Test {
 
     public static void main(String[] args) {
 
         Test test = new Test();
 
-        int[][] arr1 = new int[][]{{1, 2}, {2, 3}};
-        int[][] arr2 = new int[][]{{3, 4}, {5, 6}};
+        int[] arr1 =  {2, 4};
+        int[] arr2 =  {3};
 
-        System.out.println(arr1.length);
-        System.out.println(arr2.length);
-        System.out.println(arr1[0].length);
-
-        test.solution(arr1, arr2);
+        test.solution(5, arr1, arr2);
     }
 
-    public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr2.length];
+    public int solution(int n, int[] lost, int[] reserve) {
 
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr2.length; j++) {
-                answer[i][j] = arr1[i][j] + arr2[i][j];
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        int count = 0;
+        int answer = 0;
 
+        for (int i = 0; i < reserve.length; i++) {
+            for (int j = 0; j < lost.length; j++) {
+                if (reserve[i] == lost[j]) {
+                    reserve[i] = -1;
+                    lost[j] = -1;
+                    break;
+                }
             }
         }
+
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
+                    count++;
+                    lost[i] = - 1;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+        answer = n - lost.length + count;
         return answer;
     }
 
