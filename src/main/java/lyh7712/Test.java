390a9de0957e86202e52be3f1697c29ca259a01f
lyh7712 <mmj2rv9@kakao.com>
2021-10-05 19:24:35 +0900

    알고리즘

--- src/main/java/lyh7712/Test.java
+++ src/main/java/lyh7712/Test.java
@@ -1,9 +1,6 @@
 package lyh7712;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+
 
 public class Test {
 
@@ -11,41 +8,63 @@ public class Test {
 
         Test test = new Test();
 
-        int[] arr1 =  {2, 4};
-        int[] arr2 =  {3};
+        int[] numbers = {1,3,4,5,8,2,1,4,5,9,5};
+        String hand = "right";
 
-        test.solution(5, arr1, arr2);
+        test.solution(numbers, hand);
     }
 
-    public int solution(int n, int[] lost, int[] reserve) {
+    // 왼손 - 1, 4, 7
+    // 오른손 - 3, 6, 9
+    // 가까운 엄지손가락 - 2, 5, 8, 0
+    // 거리가 같다면 hand인 손으로 터치
+    // 누르면 answer에 hand 추가됨
+    // 손 위치는 마지막에 누른 숫자로 변경
 
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        int count = 0;
-        int answer = 0;
+    int[][] pos = {
+            {3,1}, //0
+            {0,0}, //1
+            {0,1}, //2
+            {0,2}, //3
+            {1,0}, //4
+            {1,1}, //5
+            {1,2}, //6
+            {2,0}, //7
+            {2,1}, //8
+            {2,2}  //9
+    };
 
-        for (int i = 0; i < reserve.length; i++) {
-            for (int j = 0; j < lost.length; j++) {
-                if (reserve[i] == lost[j]) {
-                    reserve[i] = -1;
-                    lost[j] = -1;
-                    break;
-                }
-            }
+    int[] left = {3,0}; // *
+    int[] right = {3,2}; // #
+    String hand;
+
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
+        this.hand = hand == "right" ? "R" : "L";
+
+        for (int num : numbers) {
+            String Umji = pushNumber(num);
+            answer += Umji;
+
+            if (Umji.equals("L")) {left = pos[num]; continue; }
+            if (Umji.equals("R")) {right = pos[num]; continue; }
         }
-
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
-                    count++;
-                    lost[i] = - 1;
-                    reserve[j] = -1;
-                    break;
-                }
-            }
-        }
-        answer = n - lost.length + count;
+        System.out.println(answer);
         return answer;
     }
 
+    private String pushNumber(int num) {
+        if (num == 1 || num == 4 || num == 7) return "L";
+        if (num == 3 || num == 6 || num == 9) return "R";
+
+        if (getDistance(num, left) < getDistance(num, right)) return "L";
+        if (getDistance(num, left) > getDistance(num, right)) return "R";
+
+        return this.hand;
+    }
+
+    //들어오는 숫자 배열에 대해 구하고자 하는 숫자 배열 왼쪽, 오른쪽을 각각 구한다.
+    private int getDistance(int num, int[] dpos) {
+        return Math.abs(dpos[0] - pos[num][0]) + Math.abs(dpos[1] - pos[num][1]);
+    }
 }
